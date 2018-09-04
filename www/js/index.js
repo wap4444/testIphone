fbdb56ce62a3c93007f928644f54050fd5f36d53
wap4444 <wap4444@mail.ru>
2018-09-04 10:32:50 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -64,15 +46,18 @@ $(document).on('click', '#getData', function (e) {
         
 
     var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
+lat=position.coords.latitude;
+long=position.coords.longitude;
+busId=localStorage.course;
+        $.ajax({type: 'POST',url: 'http://bus.controlsoft.kz/api/busSimulator.php',data:{lat:lat,long:long,budId:busId},
+success: function(data){
+alert('Координаты отправлены');
+},
+error: function(XMLHttpRequest, textStatus, errorThrown){
+alert('Ошибка, попробуйте еще раз!');
+}
+});
+};
  
     // onError Callback receives a PositionError object
     //
